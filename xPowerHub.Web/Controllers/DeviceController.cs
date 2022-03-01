b7d20b0d299c1c73bbe807e0b608a7c6a98eb448
Casper Nielsen <casperkmn@gmail.com>
2022-03-01 13:20:41 +0100

    Added Endpoint for adding new unit

--- xPowerHub.Web/Controllers/DeviceController.cs
+++ xPowerHub.Web/Controllers/DeviceController.cs
@@ -17,18 +17,31 @@ namespace xPowerHub.Web.Controllers
             _deviceManager = deviceManager;
         }
 
+        /// <summary>
+        /// Gets all the connected devices
+        /// </summary>
+        /// <returns>Array of Device</returns>
         [HttpGet("GetAll")]
         public Device[] GetAll()
         {
             return _deviceManager.GetAll().ToArray();
         }
 
+        /// <summary>
+        /// Gets the status of the given devices
+        /// </summary>
+        /// <param name="devices">The devices where there status will be search for</param>
+        /// <returns>A array of KnownStatusDevice</returns>
         [HttpPost("GetStatus")]
         public KnownStatusDevice[] PostStatus([FromBody] Device[] devices)
         {
             return _deviceManager.GetStatus(devices.ToList()).ToArray();
         }
 
+        /// <summary>
+        /// Changes the status of the device to the given status
+        /// </summary>
+        /// <param name="device">The device with the new status</param>
         [HttpPost("ChangeStatus")]
         public void ChangeStatus([FromBody] KnownStatusDevice device)
         {
--- xPowerPhoneApp/xPowerPhoneApp/Repositorys/SmartUnitRepository.cs
+++ xPowerPhoneApp/xPowerPhoneApp/Repositorys/SmartUnitRepository.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using xPowerPhoneApp.Models;
+using xPowerPhoneApp.Repositorys.Shared;
 
 namespace xPowerPhoneApp.Repositorys
 {
-    internal class SmartUnitRepository
+    internal class SmartUnitRepository : ISmartUnitRepository
     {
+        public Task<bool> AddDevice(AddDevice device)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<AddDevice>> GetDevices()
+        {
+            var device = await SharedHttpClient.Instants.Get<AddDevice>("Unit/GetNew");
+            return new List<AddDevice>() { device };
+        }
+
+        public async Task<List<AddDevice>> GetNewDevices(List<AddDevice> knownDevices)
+        {
+            var newDevices = new List<AddDevice>();
+            var devices = await this.GetDevices();
+            foreach (var device in devices)
+            {
+                if (knownDevices.Contains(device)) continue;
+                newDevices.Add(device);
+            }
+            return newDevices;
+        }
     }
 }
