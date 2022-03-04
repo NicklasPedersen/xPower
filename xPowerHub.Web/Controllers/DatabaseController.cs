940575032c4f76e62c1a6d090f3f5df4ae015437
Casper Nielsen <46668617+Casper-Nielsen@users.noreply.github.com>
2022-03-04 11:44:10 +0100

    Added a controller for power usage endpoints (#11)

---
+++ xPowerHub.Web/Controllers/DatabaseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using xPowerHub.DataStore;
+using xPowerHub.Managers.Interfaces;
+
+namespace xPowerHub.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+        private IDataStore _dataStore;
+
+        public DatabaseController(IDataStore dataStore) : base()
+        {
+            _dataStore = dataStore;
+        }
+
+        /// <summary>
+        /// Goes and starts listening for first beat
+        /// </summary>
+        /// <returns>The new Device</returns>
+        [HttpGet("reset")]
+        public void Reset()
+        {
+            if (_dataStore is DAL dal)
+            {
+                dal.DropTables();
+                dal.AddTables();
+            }
+        }
+    }
+}
