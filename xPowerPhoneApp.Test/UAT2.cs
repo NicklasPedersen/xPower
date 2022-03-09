e243efb9c4bca965ba77ab8da8dd39e460e16f6e
Nicklas Pedersen <nick8186@zbc.dk>
2022-03-09 13:14:19 +0100

    Add UAT 2, 4, and 6

---
+++ xPowerPhoneApp.Test/UAT2.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace xPowerPhoneApp.Test
+{
+    internal class UAT2 : BaseAndroidTest
+    {
+        public UAT2(Platform platform) : base(platform)
+        {
+        }
+        [Test]
+        public void See_Devices()
+        {
+            this.NavigateToSide();
+            app.WaitForElement(l => l.Marked("automationSeeUnits"), timeout: TimeSpan.FromSeconds(30));
+            app.WaitForElement(l => l.Marked("automationSeeSingleUnit"), timeout: TimeSpan.FromSeconds(30));
+            Assert.Pass("Successfull");
+        }
+        private void NavigateToSide()
+        {
+            app.Tap(c => c.Marked("automationSeeUnitsButton"));
+        }
+    }
+}
