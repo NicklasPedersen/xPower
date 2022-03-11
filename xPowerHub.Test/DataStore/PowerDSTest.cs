c6755958ad18385e259b61ddca3f4e6b764de2a5
Casper Nielsen <casperkmn@gmail.com>
2022-03-11 09:37:39 +0100

    Added test for PowerDS and for PowerManager

---
+++ xPowerHub.Test/DataStore/PowerDSTest.cs
@@ -0,0 +1,283 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xPowerHub.DataStore;
+using xPowerHub.Models;
+
+namespace xPowerHub.Test.DataStore
+{
+    public class PowerDSTest
+    {
+        private PowerDS _powerDS;
+
+        public PowerDSTest()
+        {
+            _powerDS = new PowerDS("./testdb.db");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerDS.RemoveTable();
+            _powerDS.AddTable();
+        }
+
+        [Test]
+        public async Task SaveAsync_AddPowerUsage_ShouldReturnTrue()
+        {
+            Random random = new Random();
+            PowerUsage powerUsage = new PowerUsage() { Taken = DateTime.Now, WattHour=random.NextDouble() * 100 };
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+        }
+
+        [Test]
+        public async Task SaveAsync_AddPowerUsage_CouldGetIt()
+        {
+            Random random = new Random();
+            PowerUsage? powerUsageReturn = null;
+            DateTime taken = DateTime.Now;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = random.NextDouble() * 100 };
+            
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            if(saved)
+                powerUsageReturn = await _powerDS.GetAsync(taken);
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(powerUsageReturn != null, "Got null, should have gotton a value");
+        }
+
+        [Test]
+        public async Task GetAsync_GetPowerUsage_ShouldGetIt()
+        {
+            Random random = new Random();
+            PowerUsage? powerUsageReturn = null;
+            DateTime taken = DateTime.Now;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = random.NextDouble() * 100 };
+
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+                powerUsageReturn = await _powerDS.GetAsync(taken);
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(powerUsageReturn != null, "Got null, should have gotton a value");
+
+            if (powerUsageReturn != null)
+            {
+                Assert.AreEqual(powerUsageReturn.WattHour, powerUsage.WattHour, "Got the wrong watthour value");
+                Assert.AreEqual(powerUsageReturn.Taken, powerUsage.Taken, "Got the wrong date value");
+            }
+        }
+
+        [Test]
+        public async Task GetAsync_GetUnkownPowerUsage_ShouldGetNull()
+        {
+            Random random = new Random();
+            PowerUsage? powerUsageReturn = null;
+            DateTime taken = DateTime.Now;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = random.NextDouble() * 100 };
+
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+                powerUsageReturn = await _powerDS.GetAsync(taken.AddDays(32));
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(powerUsageReturn == null, "Got value from a unknown date");
+        }
+
+        [Test]
+        public async Task GetPowerUsageHourlyAvgAsync_GetItFromOneHour_ShouldGetIt()
+        {
+            Random random = new();
+            List<PowerUsage> powerUsageReturn = new();
+            DateTime taken = DateTime.Now;
+            double wattHour = random.NextDouble() * 100;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = wattHour };
+
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+            {
+                powerUsageReturn.AddRange(await _powerDS.GetPowerUsageHourlyAvgAsync(taken));
+            }
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(powerUsageReturn.Count > 0, "Did not get a value");
+            Assert.AreEqual(powerUsageReturn[0].WattHour, wattHour, "The wattHour was not the right value");
+        }
+
+        [Test]
+        public async Task GetPowerUsageHourlyAvgAsync_GetItFromMultipleHours_ShouldGetThem()
+        {
+            Random random = new();
+            List<PowerUsage> powerUsageReturn = new();
+            DateTime taken = DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd"));
+            int NumberOfHours = random.Next(1,25);
+            double[] wattHours = new double[NumberOfHours];
+            bool[] saves = new bool[NumberOfHours];
+
+            for (int i = 0; i < NumberOfHours; i++)
+            {
+                wattHours[i] = random.NextDouble() * 100;
+                var powerUsage = new PowerUsage() { Taken = taken.AddHours(i), WattHour = wattHours[i] };
+                saves[i] = await _powerDS.SaveAsync(powerUsage);
+
+            }
+
+            powerUsageReturn.AddRange(await _powerDS.GetPowerUsageHourlyAvgAsync(taken));
+
+            for (int i = 0; i < NumberOfHours; i++)
+            {
+                Assert.IsTrue(saves[i], "Was not able to add the powerUsage");
+            }
+
+            Assert.IsTrue(powerUsageReturn.Count == NumberOfHours, "Did not get all the values");
+
+            for (int i = 0; i < NumberOfHours; i++)
+            {
+                Assert.AreEqual(powerUsageReturn[i].WattHour, wattHours[i], "The wattHour was not the right value");
+                Assert.IsTrue(saves[i], "Was not able to add the powerUsage");
+            }
+        }
+
+        [Test]
+        public async Task GetPowerUsageHourlyAvgAsync_GetFromNothing_ShouldGetEmpty()
+        {
+            List<PowerUsage> powerUsageReturn = new();
+            DateTime taken = DateTime.Now;
+
+            powerUsageReturn.AddRange(await _powerDS.GetPowerUsageHourlyAvgAsync(taken));
+
+            Assert.IsTrue(powerUsageReturn.Count == 0, "Did get some value");
+        }
+
+        [Test]
+        public async Task GetPowerUsageWeekdayAvgAsync_GetFromNothing_ShouldGetEmpty()
+        {
+            List<PowerUsage> powerUsageReturn = new();
+
+            powerUsageReturn.AddRange(await _powerDS.GetPowerUsageWeekdayAvgAsync());
+
+            Assert.IsTrue(powerUsageReturn.Count == 0, "Did get some value");
+        }
+
+        [Test]
+        public async Task GetPowerUsageWeekdayAvgAsync_GetAvgFromOne_ShouldGetavg()
+        {
+            Random random = new();
+            List<PowerUsage> powerUsageReturn = new();
+            DateTime taken = DateTime.Now;
+            double wattHour = random.NextDouble() * 100;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = wattHour };
+
+            var saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+            {
+                powerUsageReturn.AddRange(await _powerDS.GetPowerUsageWeekdayAvgAsync());
+            }
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(powerUsageReturn.Count > 0, "Did not get a value");
+            Assert.AreEqual(powerUsageReturn[0].WattHour, wattHour, "The wattHour was not the right value");
+        }
+
+        [Test]
+        public async Task GetPowerUsageWeekdayAvgAsync_GetAvgFromMultiple_ShouldGetavg()
+        {
+            Random random = new();
+            List<PowerUsage> powerUsageReturn = new();
+            DateTime taken = DateTime.Now;
+            int NumberOfWeeks = random.Next(1, 25);
+            double[] wattHours = new double[NumberOfWeeks];
+            bool[] saves = new bool[NumberOfWeeks];
+
+            for (int i = 0; i < NumberOfWeeks; i++)
+            {
+                wattHours[i] = random.NextDouble() * 100;
+                var powerUsage = new PowerUsage() { Taken = taken.AddDays(i*7), WattHour = wattHours[i] };
+                saves[i] = await _powerDS.SaveAsync(powerUsage);
+
+            }
+
+            powerUsageReturn.AddRange(await _powerDS.GetPowerUsageWeekdayAvgAsync());
+
+            for (int i = 0; i < NumberOfWeeks; i++)
+            {
+                Assert.IsTrue(saves[i], "Was not able to add the powerUsage");
+            }
+
+            Assert.IsTrue(powerUsageReturn.Count == 1, "Did get multiple values");
+
+            var avg = wattHours.Sum() / NumberOfWeeks;
+            Assert.AreEqual(powerUsageReturn[0].WattHour, avg, "The wattHour was not the right value");
+        }
+
+        [Test]
+        public async Task UpdateAsync_UpdateWatthour_ShouldReturnTrue()
+        {
+            Random random = new Random();
+            DateTime taken = DateTime.Now;
+            double newWattHour = random.NextDouble() * 100;
+            bool updated = false;
+            bool saved;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = random.NextDouble() * 100 };
+
+            saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+            {
+                powerUsage.WattHour = newWattHour;
+                updated = await _powerDS.UpdateAsync(powerUsage);
+            }
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(updated, "Was not able to update the powerUsage");
+        }
+
+        [Test]
+        public async Task UpdateAsync_UpdateWatthour_ShouldGetIt()
+        {
+            Random random = new Random();
+            PowerUsage? powerUsageReturn = null;
+            DateTime taken = DateTime.Now;
+            double newWattHour = random.NextDouble() * 100;
+            bool updated = false;
+            bool saved;
+
+            PowerUsage powerUsage = new PowerUsage() { Taken = taken, WattHour = random.NextDouble() * 100 };
+
+            saved = await _powerDS.SaveAsync(powerUsage);
+
+            if (saved)
+            {
+                powerUsage.WattHour = newWattHour;
+                updated = await _powerDS.UpdateAsync(powerUsage);
+                if (updated)
+                    powerUsageReturn = await _powerDS.GetAsync(taken);
+            }
+
+            Assert.IsTrue(saved, "Was not able to add the powerUsage");
+            Assert.IsTrue(updated, "Was not able to update the powerUsage");
+
+            if (powerUsageReturn != null)
+            {
+                Assert.AreEqual(powerUsageReturn.WattHour, powerUsage.WattHour, "Got the wrong watthour value");
+                Assert.AreEqual(powerUsageReturn.Taken, powerUsage.Taken, "Got the wrong date value");
+            }
+        }
+    }
+}
---
+++ xPowerHub.Test/Manager/PowerManagerTest.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xPowerHub.DataStore;
+using xPowerHub.Managers;
+using xPowerHub.Models;
+
+namespace xPowerHub.Test.Manager
+{
+    internal class PowerManagerTest
+    {
+        private PowerManager _powerManager;
+        private PowerDS _powerDS;
+
+        public PowerManagerTest()
+        {
+            _powerDS = new PowerDS("./testdb.db");
+            _powerManager = new PowerManager(_powerDS);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerDS.RemoveTable();
+            _powerDS.AddTable();
+        }
+
+        [Test]
+        public async Task AddUsageAsync_AddSingle_ShouldReturnTrue()
+        {
+            Random random = new Random();
+            PowerUsage powerUsage = new PowerUsage() { Taken = DateTime.Now, WattHour = random.NextDouble()*100 };
+            bool saved;
+
+            saved = await _powerManager.AddUsageAsync(powerUsage);
+
+            Assert.IsTrue(saved, "Was not able to save it");
+        }
+
+        [Test]
+        public async Task AddUsageAsync_AddMultipleWithinOneHour_ShouldReturnTrues()
+        {
+            Random random = new Random();
+            int amount = random.Next(1,25);
+            bool[] saved = new bool[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                saved[i] = await _powerManager.AddUsageAsync(new PowerUsage() { Taken = DateTime.Now.AddSeconds(i), WattHour = random.NextDouble() * 100 });
+            }
+
+            for (int i = 0; i < amount; i++)
+            {
+                Assert.IsTrue(saved[i], "Was not able to save it");
+            }
+        }
+
+        [Test]
+        public async Task GetpowerUsageTodayAvgAsync_GetItFromOne_ShouldReturnTheWattHour()
+        {
+            Random random = new Random();
+            PowerUsage powerUsage = new PowerUsage() { Taken = DateTime.Now, WattHour = random.NextDouble() * 100 };
+            double returnValue;
+            bool saved;
+
+            saved = await _powerManager.AddUsageAsync(powerUsage);
+
+            returnValue = await _powerManager.GetpowerUsageTodayAvgAsync();
+
+            Assert.IsTrue(saved, "Was not able to save it");
+            Assert.AreEqual(returnValue, powerUsage.WattHour, "Did not Get the same watthour");
+        }
+
+        [Test]
+        public async Task GetpowerUsageTodayAvgAsync_GetItFromMultiple_ShouldReturnTheSum()
+        {
+            Random random = new Random();
+            int amount = random.Next(1, 25);
+            double[] wattHours = new double[amount];
+            bool[] saved = new bool[amount];
+            double returnValue;
+
+            for (int i = 0; i < amount; i++)
+            {
+                wattHours[i] = random.NextDouble()*10;
+                saved[i] = await _powerManager.AddUsageAsync(new PowerUsage() { Taken = DateTime.Now.AddSeconds(i), WattHour = wattHours[i] });
+            }
+
+            returnValue = await _powerManager.GetpowerUsageTodayAvgAsync();
+
+            for (int i = 0; i < amount; i++)
+            {
+                Assert.IsTrue(saved[i], "Was not able to save it");
+            }
+
+            var sum = wattHours.Sum();
+            Assert.AreEqual(returnValue, sum, "Did not get the sum of the power usage");
+        }
+
+        [Test]
+        public async Task GetpowerUsageTodayAvgAsync_GetItFromNothing_ShouldReturnZero()
+        {
+            double returnValue;
+
+            returnValue = await _powerManager.GetpowerUsageTodayAvgAsync();
+
+            Assert.AreEqual(returnValue, 0, "Did not get zero");
+        }
+    }
+}
